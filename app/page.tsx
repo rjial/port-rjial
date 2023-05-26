a25ca874c5e8e158beddd2ebc3bbf51e4401a60f
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-05-27 01:12:05 +0700

    add responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <main className="">
       <div className='h-screen px-16 mx-auto pt-24 flex items-center justify-between'>
-        <div className='w-1/2'>
+        <div className='lg:w-1/2'>
           <h1 className='text-white font-semibold text-5xl mb-6'>Hi!, I'm Rizal Abdul Basith </h1>
           <h4 className='text-gray-400 font-medium leading-8'>I am a passionate and experienced full-stack developer based in Malang, Indonesia. </h4>
           <h4 className='text-gray-400 font-medium leading-8 mb-9'>With expertise in PHP, JavaScript, Laravel, React, and Vue, I specialize in crafting dynamic and efficient web applications.</h4>
-          <div className='flex items-center space-x-7'>
+          <div className='flex items-center space-x-7 justify-between lg:justify-normal'>
             <button className='px-5 py-2.5 bg-sky-600 rounded-md text-white font-semibold transition-colors hover:bg-sky-500 duration-100'>Hire Me!</button>
             <div className='flex items-center space-x-4'>
               <a target='_blank' href="https://www.linkedin.com/in/rjial/"><BsLinkedin className='text-2xl text-white hover:shadow-md hover:shadow-white/20 transition-shadow'/></a>
@@ -19,7 +19,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div>
+        <div className='hidden lg:block'>
           <Image src={dp} alt="display picture" priority={false} loading='lazy' className='h-96 w-96 rounded-full' unoptimized={true} />
         </div>
       </div>
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,14 +1,33 @@
+'use client'
+
 import Link from "next/link"
+import { useState } from "react"
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 export default () => {
+  const [showNav, setShowNav] = useState<boolean>(false);
   return (
     <div className="fixed w-full">
       <div className="flex justify-between px-16 py-10 items-center ">
         <h1 className="text-white font-extrabold">rjial</h1>
-        <div className="flex justify-between space-x-3 text-white">
+        <div className="lg:flex lg:justify-between space-x-3 text-white hidden">
           <Link href="/"><div className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">Home</div></Link>
           <Link href="/about"><div className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">About</div></Link>
           <Link href="/contact"><div className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">Contact</div></Link>
+        </div>
+        <div className="block lg:hidden relative">
+          <button onClick={() => setShowNav(!showNav)} className="py-2 px-4 z-50  text-white hover:bg-white/10 rounded-md transition ease-in-out ">
+            <RxHamburgerMenu className="text-white text-2xl" />
+          </button>
+          
+          {showNav ? <><div onClick={() => setShowNav(!showNav)} className="absolute w-screen h-screen -right-16 -top-10 z-30">
+          </div><div className="absolute bottom-120 w-screen -right-16 mt-3 z-40">
+            <div className="bg-[#1e1e1e] outline outline-1 outline-zinc-800 mx-16 p-4 rounded-md text-white flex flex-col space-y-3">
+              <Link onClick={() => setShowNav(!showNav)} href="/" className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">Home</Link>
+              <Link onClick={() => setShowNav(!showNav)} href="/about" className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">About</Link>
+              <Link onClick={() => setShowNav(!showNav)} href="/contact" className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">Contact</Link>
+            </div>
+          </div></> : <></>}
         </div>
       </div>
     </div>
