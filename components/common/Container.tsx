59303d4e0994d386509f89de24dac500c137eabc
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-05-27 18:25:23 +0700

    add animatepresence

--- components/common/Container.tsx
+++ components/common/Container.tsx
@@ -1,5 +1,18 @@
 'use client'
 import React from "react";
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter, usePathname } from "next/navigation";
 
-export default ({ children }: { children: React.ReactNode }) => <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} exit={{opacity: 0, y: 20}} id="container">{children}</motion.div>
+export default ({ children }: { children: React.ReactNode }) => {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0, transition: { ease: "easeInOut" } }}
+        exit={{ opacity: 0, y: 20 }}
+        id="container">
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -4,12 +4,23 @@ import Link from "next/link"
 import React, { useState } from "react"
 import { RxHamburgerMenu } from 'react-icons/rx'
 
-const MenuItemResp = ({children, onClick, href, className}: {children: React.ReactNode, onClick: React.MouseEventHandler<HTMLAnchorElement>, href: string, className: string | undefined}) => {
-  return <Link onClick={onClick} href={href} className={className}>{children}</Link>
+const MenuItemResp = ({children, onClick, href}: {children: React.ReactNode, onClick: React.MouseEventHandler<HTMLAnchorElement>, href: string}) => {
+  return <Link onClick={onClick} href={href} className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">{children}</Link>
 }
 const MenuItem = ({children, href}: {children: React.ReactNode, href: string}) => {
   return <Link href={href} className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">{children}</Link>
 }
+
+export interface MenuDataType {
+  id: number,
+  name: string,
+  path: string
+}
+export const menuData: Array<MenuDataType> = [
+  {id: 0, name: "Home", path: "/"},
+  {id: 1, name: "About", path: "/about"},
+  {id: 2, name: "Contact", path: "/contact"},
+]
 
 export default () => {
   const [showNav, setShowNav] = useState<boolean>(false);
@@ -18,9 +29,7 @@ export default () => {
       <div className="flex justify-between px-16 py-10 items-center ">
         <h1 className="text-white font-extrabold">rjial</h1>
         <div className="lg:flex lg:justify-between space-x-3 text-white hidden">
-          <MenuItem href="/">Home</MenuItem>
-          <MenuItem href="/about">About</MenuItem>
-          <MenuItem href="/contact">Contact</MenuItem>
+          {menuData.map(item => <MenuItem href={item.path}>{item.name}</MenuItem>)}
         </div>
         <div className="block lg:hidden static">
           <button onClick={() => setShowNav(!showNav)} className="py-2 px-4 z-50  text-white hover:bg-white/10 rounded-md transition ease-in-out ">
@@ -30,9 +39,7 @@ export default () => {
           {showNav ? <><div onClick={() => setShowNav(!showNav)} className="absolute w-screen h-screen left-0 top-0 z-30">
           </div><div className="absolute bottom-120 w-screen right-0 mt-3 z-40">
             <div className="bg-[#1e1e1e] outline outline-1 outline-zinc-800 mx-16 p-4 rounded-md text-white flex flex-col space-y-3">
-              <MenuItemResp onClick={() => setShowNav(!showNav)} href="/" className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">Home</MenuItemResp>
-              <MenuItemResp onClick={() => setShowNav(!showNav)} href="/about" className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">About</MenuItemResp>
-              <MenuItemResp onClick={() => setShowNav(!showNav)} href="/contact" className="py-2 px-4 hover:bg-white/10 rounded-md transition ease-in-out">Contact</MenuItemResp>
+              {menuData.map(item => <MenuItemResp onClick={() => setShowNav(!showNav)} href={item.path}>{item.name}</MenuItemResp>)}
             </div>
           </div></> : <></>}
         </div>
